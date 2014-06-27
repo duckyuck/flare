ada5420af1713e8f18c72a28f179aa92a0d3ee9a
Anders Furseth <anders.furseth@gmail.com>
2014-06-27 19:34:02 +0200

    Eating our own dog food :)

--- project.clj
+++ project.clj
@@ -5,4 +5,6 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojars.brenton/google-diff-match-patch "0.1"]
-                 [clansi "1.0.0"]])
+                 [clansi "1.0.0"]]
+  :profiles {:dev {:injections [(require 'flare.clojure-test)
+                                (flare.clojure-test/install!)]}})
