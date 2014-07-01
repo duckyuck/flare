50808d0b08878f591b6b2511838a5ae422bcc56b
Anders Furseth <anders.furseth@gmail.com>
2014-07-01 17:57:16 +0200

    Adds integration with midje.

--- project.clj
+++ project.clj
@@ -5,6 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojars.brenton/google-diff-match-patch "0.1"]
+                 [midje "1.6.3"]
                  [clansi "1.0.0"]]
   :profiles {:dev {:injections [(require 'flare.clojure-test)
                                 (flare.clojure-test/install!)]}}
---
+++ src/flare/midje.clj
@@ -0,0 +1,28 @@
+(ns flare.midje
+  (:require [flare.core :refer [diff generate-reports]]
+            [midje.emission.plugins.default :as default]
+            [midje.emission.plugins.util :as util]
+            [midje.emission.state :as state]))
+
+(defn install! []
+
+  (defn emit-diff-reports
+    [reports]
+    (util/emit-one-line "")
+    (doseq [report reports]
+      (util/emit-one-line report)))
+
+  (def default-fail-emissioner (:fail default/emission-map))
+
+  (defn flare-fail-emissioner
+    [failure]
+    (default-fail-emissioner failure)
+    (when (= (:type failure) :actual-result-did-not-match-expected-value)
+      (-> (diff (:actual failure) (:expected-result failure))
+          generate-reports
+          emit-diff-reports)))
+
+  (def emission-map (assoc default/emission-map
+                      :fail flare-fail-emissioner))
+
+  (state/install-emission-map-wildly emission-map))
