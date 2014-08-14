503292a80be58f113aa0e47c7225f68552f0827f
Anders Furseth <anders.furseth@gmail.com>
2014-08-14 20:16:59 +0200

    Adds options param to clojure-test/install!

--- src/flare/clojure_test.clj
+++ src/flare/clojure_test.clj
@@ -1,36 +1,46 @@
 (ns flare.clojure-test
-  (:require [flare.core :refer [diff generate-reports]]
+  (:require [flare.core :as flare]
             [clojure.test :as ct]))
 
-(defn install! []
+(defn install!
+  ([]
+     (install! {}))
+  ([opts]
+     (defn print-diff
+       [diff]
+       (let [reports (flare/generate-reports diff)]
+         (println "")
+         (doseq [report reports]
+           (println report))))
 
-  (defn print-diff
-    [diff]
-    (let [reports (generate-reports diff)]
-      (println "")
-      (doseq [report reports]
-        (println report))))
+     (defmethod ct/report :fail [m]
+       (ct/with-test-out
+         (ct/inc-report-counter :fail)
+         (println "\nFAIL in" (ct/testing-vars-str m))
+         (when (seq ct/*testing-contexts*) (println (ct/testing-contexts-str)))
+         (when-let [message (:message m)] (println message))
+         (println "expected:" (pr-str (:expected m)))
+         (println "  actual:" (pr-str (:actual m)))
+         (if-let [diff (::difference m)]
+           (print-diff (::difference m)))))
 
-  (defmethod ct/report :fail [m]
-    (ct/with-test-out
-      (ct/inc-report-counter :fail)
-      (println "\nFAIL in" (ct/testing-vars-str m))
-      (when (seq ct/*testing-contexts*) (println (ct/testing-contexts-str)))
-      (when-let [message (:message m)] (println message))
-      (println "expected:" (pr-str (:expected m)))
-      (println "  actual:" (pr-str (:actual m)))
-      (if-let [diff (::difference m)]
-        (print-diff (::difference m)))))
+     (defn diff
+       [args]
+       (when (= 2 (count args))
+         (let [args (if (= (:expected opts) :first)
+                      (reverse args)
+                      args)]
+           (apply flare/diff args))))
 
-  (defmethod ct/assert-expr '=
-    [msg form]
-    (let [args (rest form)]
-      `(let [args# (list ~@args)
-             result# (apply = args#)]
-         (if result#
-           (ct/do-report {:type :pass, :message ~msg,
-                          :expected '~form, :actual (cons '= args#)})
-           (ct/do-report {:type :fail, :message ~msg,
-                          :expected '~form, :actual (list '~'not (cons '~'=== args#))
-                          ::difference (apply diff args#)}))
-         result#))))
+     (defmethod ct/assert-expr '=
+       [msg form]
+       (let [args (rest form)]
+         `(let [args# (list ~@args)
+                result# (apply = args#)]
+            (if result#
+              (ct/do-report {:type :pass, :message ~msg,
+                             :expected '~form, :actual (cons '= args#)})
+              (ct/do-report {:type :fail, :message ~msg,
+                             :expected '~form, :actual (list '~'not (cons '~'=== args#))
+                             ::difference (diff args#)}))
+            result#)))))
