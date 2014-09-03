1fa787963b6a7b399257ff3f63c10a9951a2df5b
Anders Furseth <anders.furseth@gmail.com>
2014-09-04 00:29:28 +0200

    Unused, deplying via 'lein deploy' instead..

--- deploy-clojars.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-for MODULE in flare-core flare-clojure-test flare-midje
-do
-  cd $MODULE
-  lein do clean, test, jar, pom
-  scp pom.xml target/flare-*.jar clojars@clojars.org:
-  cd ..
-done
