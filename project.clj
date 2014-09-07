e38093fcf162c5df5660e21af8d1d88e8c6399b3
Anders Furseth <anders.furseth@gmail.com>
2014-09-07 21:05:16 +0200

    lein-release plugin: bumped version from 0.2.4 to 0.2.5-SNAPSHOT for next development cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject flare "0.2.4"
+(defproject flare "0.2.5-SNAPSHOT"
   :description "Enlightened failure reports for clojure.test"
   :url "http://github.com/andersfurseth/flare"
   :license {:name "Eclipse Public License"
